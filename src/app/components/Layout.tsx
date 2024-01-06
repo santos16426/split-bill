034eb1e90a5c35a41ab975a189f6cb33e3e9a1be
Billy Joe Santos <santos16426@gmail.com>
2024-01-07 01:58:22 +0800

    style: updated ui structure

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -11,17 +11,16 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-  } from "@/app/components/ui/select"
-import Group from './Group';
+} from "@/app/components/ui/select"
 import Link from 'next/link';
-  
+
 const quicksand = Quicksand({ subsets: ['latin'] });
 
 interface LayoutProps {
     children: ReactNode;
-} 
-const Layout:React.FC<LayoutProps> = ({ children }) => {
-    const {settingsContext} = useAppContext();
+}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const { settingsContext } = useAppContext();
     return (
         <div className={cn("relative h-full font-sans antialiased", quicksand.className)}>
             <Head>
@@ -29,24 +28,24 @@ const Layout:React.FC<LayoutProps> = ({ children }) => {
                 <meta name="description" content='' />
             </Head>
             <main className='relative flex flex-col min-h-screen'>
-                <div className='flex-grow flex-1 flex flex-row relative'>
-                    <div className='absolute z-99 w-full h-20 flex flex-row justify-around px-20 py-5 bg-white bg-opacity-10 items-center'>
+                <div className='flex-grow flex-1 flex flex-col relative'>
+                    <div className='absolute z-99 w-full h-20 flex flex-row justify-between px-32 py-5 bg-white bg-opacity-10 items-center'>
                         <Link href="/">
                             <div className='flex flow-row gap-2 items-center text-black cursor-pointer'>
-                                <p className='text-3xl font-bold text-red-400'>Split Bill</p>
-                                <div className='text-red-400'>
+                                <p className='text-3xl font-bold text-teal-400'>Split Bill</p>
+                                <div className='text-teal-400'>
                                     <Coins />
                                 </div>
                             </div>
                         </Link>
-                        <div/>
+                        <div />
                         <div className='flex flex-row gap-5 text-black items-center font-bold'>
-                            <Select 
-                                value={settingsContext.settings.currency} 
-                                onValueChange={(e:string):void=>settingsContext.setSettings({currency:e})}                            
+                            <Select
+                                value={settingsContext.settings.currency}
+                                onValueChange={(e: string): void => settingsContext.setSettings({ currency: e })}
                             >
                                 <SelectTrigger className="w-[70px]">
-                                    <SelectValue placeholder="Currency"/>
+                                    <SelectValue placeholder="Currency" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectItem className='font-bold' value="₱">PHP</SelectItem>
@@ -55,18 +54,15 @@ const Layout:React.FC<LayoutProps> = ({ children }) => {
                             </Select>
                         </div>
                     </div>
-                    <div className='w-1/4 h-full'></div>
-                    <div className='w-full pt-24 px-5 h-screen'>
-                        <div className='mb-5'>
-                            <Group/>
-                        </div>
+
+                    <div className='pt-24 px-32'>
                         {children}
                     </div>
-                    <div className='w-1/4 h-full'></div>
+
                     <div className='absolute bottom-0 w-full flex flex-row justify-center items-center text-white text-xs'>Copyright 2024 Billy Joe Santos</div>
                 </div>
             </main>
-            
+
         </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
-import Dashboard from '@/app/components/Dashboard';
+
+import Expense from '@/app/components/Expense';
 import Group from '@/app/components/Group';
+import Members from '@/app/components/Members';
+import { Separator } from '@/app/components/ui/separator';
 import { useAppContext } from '@/app/context/AppContext';
 import { cn } from '@/app/lib/utils';
 
-
-
 const App = () => {
-  const {settingsContext} = useAppContext();
+  const { activeGroupContext } = useAppContext();
+  const { activeGroup } = activeGroupContext
   return (
-      <div className={cn('flex flex-col')}>
-        <Dashboard/>
+    <div className={cn('flex flex-row justify-between w-full gap-3', !activeGroup && 'justify-center')}>
+      <div className={cn('flex flex-col gap-3 mb-5 w-1/3 h-full bg-white bg-opacity-10 p-3 rounded-md shadow-lg')}>
+        <Group />
       </div>
+      <div className={cn('mb-5 w-full bg-white bg-opacity-30 p-3 rounded-md shadow-lg', !activeGroup ? 'h-full' : 'h-fit')}>
+        <Expense />
+      </div>
+      <div className={cn('mb-5 w-1/3 bg-white bg-opacity-10  h-fit rounded-md shadow-lg transition-max-w duration-500 ease-in-out', !activeGroup ? 'max-w-0 overflow-hidden' : 'max-w-full p-3')}>
+        <Members />
+      </div>
+
+    </div >
   );
 };
 
